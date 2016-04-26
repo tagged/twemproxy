b8c203510a4175124010b9536343ed755caa4302
Misha Nasledov <misha@nasledov.com>
2016-04-25 22:36:32 -0700

    remove dupe

--- src/nc_conf.c
+++ src/nc_conf.c
@@ -1387,25 +1387,6 @@ conf_validate_server(struct conf *cf, struct conf_pool *cp)
         return NC_ERROR;
     }
 
-    array_sort(&cp->server, conf_server_name_cmp);
-    for (valid = true, i = 0; i < nserver - 1; i++) {
-        struct conf_server *cs1, *cs2;
-
-        cs1 = array_get(&cp->server, i);
-        cs2 = array_get(&cp->server, i + 1);
-
-        if (string_compare(&cs1->name, &cs2->name) == 0) {
-            log_error("conf: pool '%.*s' has servers with same name '%.*s'",
-                      cp->name.len, cp->name.data, cs1->name.len,
-                      cs1->name.data);
-            valid = false;
-            break;
-        }
-    }
-    if (!valid) {
-        return NC_ERROR;
-    }
-
     return NC_OK;
 }
 
