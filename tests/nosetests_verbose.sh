eaae2c53efeb0c0000cb05cb6a096475ddd03e9d
Tyson Andre <tysonandre775@hotmail.com>
2021-04-09 16:02:21 -0400

    Start using github workflow to build branches and PRs
    Run in multiple redis versions
    
    Update some unit tests and reorganize docker build steps

--- tests/nosetests_verbose.sh
+++ tests/nosetests_verbose.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xeu
+# A simple utility script to run tests with extremely verbose output.
 
 if [[ $# == 0 ]]; then
 	echo "Usage: $0 test_a [test_b, ...]" 1>&2
@@ -8,8 +9,4 @@ fi
 # Print test logging to stderr
 export T_LOGFILE=-
 
-if which python3.6 > /dev/null; then
-	python3.6 -m nose -v --nologcapture --nocapture "$@"
-else
-	python3 -m nose -v --nologcapture --nocapture "$@"
-fi
+python3 -m nose -v --nologcapture --nocapture "$@"
