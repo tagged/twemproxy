aca437998d5fa76c98aad4c2d20993fe2249afdc
Tyson Andre <tysonandre775@hotmail.com>
2021-04-20 12:45:17 -0400

    Test the result of the ketama_hash function

--- src/test_all.c
+++ src/test_all.c
@@ -19,6 +19,8 @@ static void test_hash_algorithms(void) {
     // refer to libmemcached tests/hash_results.h
     expect_same_uint32_t(2297466611U, hash_one_at_a_time("apple", 5), "should have expected one_at_a_time hash for key \"apple\"");
     expect_same_uint32_t(3195025439U, hash_md5("apple", 5), "should have expected md5 hash for key \"apple\"");
+    expect_same_uint32_t(3853726576U, ketama_hash("server1-8", strlen("server1-8"), 0), "should have expected ketama_hash for server1-8 index 0");
+    expect_same_uint32_t(2667054752U, ketama_hash("server1-8", strlen("server1-8"), 3), "should have expected ketama_hash for server1-8 index 3");
 }
 
 static void test_config_parsing(void) {
